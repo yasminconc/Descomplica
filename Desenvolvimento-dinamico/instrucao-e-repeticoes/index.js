75112a31365141c76d92143505a18c91f8ee7e99
Yasmin Silva da Conceição <yasmisilvaconc@gmail.com>
2025-08-14 20:01:21 -0300

    Laços de repetição

---
+++ Desenvolvimento-dinamico/instrucao-e-repeticoes/index.js
@@ -0,0 +1,12 @@
+
+
+let weatherForecast = 'rainy'
+
+//Analogia com a vida real: Decidindo se devemos levar um guarda-chuva
+
+if(weatherForecast === 'rainy') {
+    console.log('Levar guarda chuva')
+    
+}else {
+    console.log('Não levar guarda chuva')
+}
